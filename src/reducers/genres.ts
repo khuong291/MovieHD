457ee501482678f96605dfe44d5c0f84a6378379
khuong291 <dkhuong291@gmail.com>
2019-08-16 11:26:08 +0700

    Add gh page

--- src/reducers/genres.ts
+++ src/reducers/genres.ts
@@ -1,5 +1,5 @@
-import { MovieGenre } from "src/apis/movies";
 import { ReduxAction, SAVE_GENRES } from "src/actions/genres";
+import { MovieGenre } from "src/apis/movies";
 
 export const genres = (state: MovieGenre[] = [], action: ReduxAction) => {
   switch (action.type) {
